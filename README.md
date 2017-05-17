4ea334f4af0d44e932ad12770b7de5ccd9d7bbf8
jwma <mjw@majiawei.com>
2017-05-17 17:08:15 +0800

    初始化项目，安装需要的 Bundle 和搭建基础结构

---
+++ README.md
@@ -0,0 +1,4 @@
+vwu-gaming
+==========
+
+A Symfony project created on May 17, 2017, 3:41 pm.
---
+++ src/AppBundle/Controller/Admin/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class MainController
+ * @package AppBundle\Controller\Admin
+ */
+class MainController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+    }
+}
